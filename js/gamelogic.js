8c28d80939167ffcf1a0e09d2d27e69fa4dcf318
Dennis <54860429+denniswoodrow@users.noreply.github.com>
2023-01-26 14:20:02 -0600

    let playRound() get case-insensitive user input
    made playRound() acquire user input that is case-insensitive
    to compare against the strings in choices array which was
    made accessible via global scope

--- js/gamelogic.js
+++ js/gamelogic.js
@@ -1,25 +1,30 @@
-// User inputs one of three choices.
-// The computer generates one of three choices.
-// The user choice and the computer choice are compared.
-// If the user choice beats the computer choice then the user score += 1.
-// If the computer choice beats the user choice then the computer score += 1.
-// Each round compares the user score and the computer score.
-// If one score === 5 then the game is over.
+// User inputs one of three choices
+// The computer generates one of three choices
+// The user choice and the computer choice are compared
+// If the user choice beats the computer choice then the user score += 1
+// If the computer choice beats the user choice then the computer score += 1
+// Each round compares the user score and the computer score
+// If one score === 5 then the game is over
 
+let choices = ["Rock", "Paper", "Scissors"];
 
-// simulate computer choice by randomly returning "Rock", "Paper", or "Scissors"
+// Simulate computer choice by randomly returning "Rock", "Paper", or "Scissors"
 function getComputerChoice() {
-    // what the computer can choose from
-    let choices = ["Rock", "Paper", "Scissors"];
-    
-    // chooses a random number between 0 and 2
+    // Choose a random number between 0 and 2
     function chooseRandomNumber() {
         return Math.floor(Math.random() * 3);
     }
     
-    // what the computer chooses
+    // What the computer chose
     let computerChoice = chooseRandomNumber();
 
-    //return the computer's choice
+    // Return the computer's choice
     return choices[computerChoice];
 }
+
+// Play a single round of rock, paper, scissors
+function playRound(playerSelection, computerSelection) {
+    // Acquire case-insensitive user input
+    let userInput = prompt("Enter your choice: Rock, Paper, Scissors").toUpperCase();
+}
+
